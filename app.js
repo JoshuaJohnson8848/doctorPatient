0a60c71c710fbb1d5405d8e186c3ef280ce3786a
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-12-22 07:08:18 +0530

    Added JSON Parser in App.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+app.use(express.json());
+
 dotenv.config({ path: './config/.env' });
 
 mongoose.set('strictQuery', true);
