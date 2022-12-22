60dfce74d042c5f920f6df431bf2567b5ba11153
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-12-22 08:44:52 +0530

    Added Controller Function for Patient Login

--- controller/patient/auth.js
+++ controller/patient/auth.js
@@ -1,1 +1,39 @@
-exports.signup = (req, res, next) => {};
+const Patient = require('../../models/patients');
+const bcrypt = require('bcryptjs');
+
+exports.signup = async (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const phone = req.body.phone;
+  try {
+    const existingEmail = await Patient.findOne({ email: email });
+    if (existingEmail) {
+      const error = new Error('Email Already Exist');
+      error.status = 422;
+      throw error;
+    }
+    const hashPass = await bcrypt.hash(password, 12);
+    if (!hashPass) {
+      const error = new Error('Password Not Hashed Something Went Wrong');
+      error.status = 422;
+      throw error;
+    }
+    const patient = new Patient({
+      name: name,
+      email: email,
+      password: hashPass,
+      phone: phone,
+    });
+    const createdPatient = await patient.save();
+    res.status(200).json({
+      message: 'Patient Successfully Created',
+      patient: createdPatient,
+    });
+  } catch (err) {
+    if (!err.status) {
+      err.status = 500;
+    }
+    next(err);
+  }
+};
