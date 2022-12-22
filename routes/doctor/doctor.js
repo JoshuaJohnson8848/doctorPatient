763cedf53cff34c783e86dc4644f0b83029adcc1
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-12-22 23:43:03 +0530

    Finished Working of Fetch One Doctor Feature

--- routes/doctor/doctor.js
+++ routes/doctor/doctor.js
@@ -4,6 +4,6 @@ const doctorController = require('../../controller/doctor/doctor');
 
 router.get('', doctorController.getAll);
 
-router.get('/:id');
+router.get('/:id', doctorController.getById);
 
 module.exports = router;
