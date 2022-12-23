a03bfe8e3e37252704a52d7984d1a746eb9fe0bc
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-12-23 17:47:36 +0530

    Added Auth Middleware for Doctors Authentication

---
+++ middleware/isAuthD.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+  try {
+    const authHeader = await req.get('Authorization');
+    if (!authHeader) {
+      const error = new Error('Not Authorized');
+      error.status = 401;
+      throw error;
+    }
+    const token = await authHeader.split(' ')[1];
+    if (!token) {
+      const error = new Error('Not Authorized');
+      error.status = 401;
+      throw error;
+    }
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+    if (!decodedToken) {
+      const error = new Error('Not Authorized');
+      error.status = 401;
+      throw error;
+    }
+    req.doctorId = decodedToken.doctorId;
+    next();
+  } catch (err) {
+    if (!err.status) {
+      err.status = 500;
+    }
+    next(err);
+  }
+};
