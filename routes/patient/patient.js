a6b7bcddf9bbd0c61cd39b0c89b1924a5ffd8497
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-12-23 15:43:59 +0530

    Added Router for Patient-getAll Feature

---
+++ routes/patient/patient.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const patientController = require('../../controller/patient/patient');
+
+router.get('');
+
+module.exports = router;
