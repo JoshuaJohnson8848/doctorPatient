2a7cb345942ace5c75c8f76eeec5a7f51ec6f2e2
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-12-23 15:57:27 +0530

    Finished Working of Patient-getAll Feature

--- routes/patient/patient.js
+++ routes/patient/patient.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../../controller/patient/patient');
 
-router.get('');
+router.get('', patientController.getAll);
 
 module.exports = router;
